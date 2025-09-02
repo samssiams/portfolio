4df2206902c2127e274524bc72402a91deb081a6
samuelcruz-kyna <sam.cruz.kyna@gmail.com>
2025-09-02 13:15:40 +0800

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,9 +3,12 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import { navLinks } from "@/routers/router";
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
     <header className="fixed top-0 left-1/2 z-10 w-full -translate-x-1/2 bg-[#232732]/50 backdrop-blur-sm py-1">
       <div className="mx-auto flex w-full max-w-[900px] items-center justify-between px-6 py-2 text-[20px] font-bold">
@@ -23,16 +26,28 @@ const Header = () => {
 
         {/* Navigation */}
         <nav className="flex gap-9 text-[16px] font-semibold tracking-[0.38px]">
-          {navLinks.map(({ label, href }) => (
-            <Link
-              key={href}
-              href={href}
-              className="group relative text-white"
-            >
-              <span>{label}</span>
-              <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-white transition-all duration-300 group-hover:w-full" />
-            </Link>
-          ))}
+          {navLinks.map(({ label, href }) => {
+            const isActive = pathname === href;
+            const isHome = href === "/" && pathname === "/";
+
+            return (
+              <Link
+                key={href}
+                href={href}
+                className="group relative text-white"
+              >
+                <span>{label}</span>
+
+                {/* Show underline only if NOT active Home */}
+                {!isHome && (
+                  <span
+                    className={`absolute left-0 bottom-0 h-[2px] bg-white transition-all duration-300 
+                    ${isActive ? "w-full" : "w-0 group-hover:w-full"}`}
+                  />
+                )}
+              </Link>
+            );
+          })}
         </nav>
       </div>
     </header>
